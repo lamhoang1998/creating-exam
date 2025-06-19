397801b65970aa9fa4f7c7ce07ee69e1fce9023e
Lam <lamkyo373@gmail.com>
2025-06-19 14:58:35 +0700

    done with creating button to switch between forms

--- frontend/src/Form/CreateExamQuestion.tsx
+++ frontend/src/Form/CreateExamQuestion.tsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import style from "./CreateExamQuestion.module.css";
+import MultipleChoice from "./MultipleChoice";
+import ProblemSolvingQuestion from "./ProblemSolvingQuestion";
 
 function CreateExamQuestion() {
+	const [multipleChoice, setMultipleChoice] = useState(true);
+	const [problemSolving, setProblemSolving] = useState(false);
+
+	function handleMultipleChoice() {
+		if (!multipleChoice) {
+			setMultipleChoice(true);
+			setProblemSolving(false);
+		}
+	}
+
+	function handleProblemSolving() {
+		if (!problemSolving) {
+			setProblemSolving(true);
+			setMultipleChoice(false);
+		}
+	}
+
 	return (
 		<div>
 			<h2>Fill the form to create questions. choose one </h2>
 			<form>
 				<div className={style.createExamSwitch}>
-					<span>Multiple choice</span>
-					<span>Problem solving</span>
+					<span onClick={handleMultipleChoice}>Multiple choice</span>
+					<span onClick={handleProblemSolving}>Problem solving</span>
 				</div>
+				{multipleChoice && <MultipleChoice />}
+				{problemSolving && <ProblemSolvingQuestion />}
 			</form>
 		</div>
 	);
