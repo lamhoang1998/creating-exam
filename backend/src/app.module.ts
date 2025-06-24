b52a2ffc5ffbe9cabf2b02c8bbb6eaa9c15d76e9
Lam <lamkyo373@gmail.com>
2025-06-24 14:32:39 +0700

    create the routing system for user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from './common/prisma/init.prisma';
 import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService, JwtService],
 })
